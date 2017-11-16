b9eb8658c3d4c65f70564082966e5c28b616d081
HashGate <dev@hashgate.eu>
2017-11-16 17:38:10 +0100

    Init

---
+++ website/config.js
@@ -0,0 +1,15 @@
+var api = "http://your.server:8117";
+
+var poolHost = "your.server";
+
+var email = "support@your.server";
+
+var cryptonatorWidget = ["HC-BTC", "HC-USD", "HC-EUR", "HC-GBP"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://explorer.hashgate.eu/?hash=";
+
+var transactionExplorer = "http://explorer.hashgate.eu/?hash=";
+
+var themeCss = "themes/nightly-mining-dark-theme.css";
